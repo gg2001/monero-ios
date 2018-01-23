756b5077c7976e9e3cb4fa9ebfd32fe95889060c
gg2001 <gautham.gg@gmail.com>
2018-01-23 15:03:05 +0530

    JavaScript embedding

--- monero/NewWallet.swift
+++ monero/NewWallet.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import JavaScriptCore
 
 class NewWallet: UIViewController {
+    
+    var jsContext: JSContext!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,5 +23,33 @@ class NewWallet: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.initializeJS()
+        self.helloWorld()
+    }
     
+    func initializeJS() {
+        self.jsContext = JSContext()
+        
+        if let jsSourcePath = Bundle.main.path(forResource: "NewWallet", ofType: "js") {
+            do {
+                // Load its contents to a String variable.
+                let jsSourceContents = try String(contentsOfFile: jsSourcePath)
+                
+                // Add the Javascript code that currently exists in the jsSourceContents to the Javascript Runtime through the jsContext object.
+                self.jsContext.evaluateScript(jsSourceContents)
+            }
+            catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    func helloWorld() {
+        if let variableHelloWorld = self.jsContext.objectForKeyedSubscript("helloWorld") {
+            print(variableHelloWorld.toString())
+        }
+    }
 }
